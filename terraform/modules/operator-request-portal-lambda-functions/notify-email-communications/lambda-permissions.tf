4394c0d085d1bad6597b36ec29bc965028b89ca5
Alec Ashmore <alec.ashmore@digital.cabinet-office.gov.uk>
2026-01-06 23:11:01 +0000

    Add email notification functions
    Adds the email notification and communications Lambda functions module.
    
    This module handles email notifications via notify for operator portal events and
    communications workflows.

---
+++ terraform/modules/operator-request-portal-lambda-functions/notify-email-communications/lambda-permissions.tf
@@ -0,0 +1,13 @@
+resource "aws_lambda_permission" "lambda_invoke_permission" {
+  for_each = toset([
+    "${var.environment}-log-upload-handler",
+    "${var.environment}-notify-on-upload",
+    "${var.environment}-certificate-generation-lambda"
+  ])
+
+  statement_id  = "AllowInvokeFrom-${each.key}"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.notify_service_lambda.function_name
+  principal     = "lambda.amazonaws.com"
+  source_arn    = "arn:aws:lambda:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:function:${each.key}"
+}
